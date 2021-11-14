1ce59df7ba725dab6e0f7609006d30beeec4524b
Matthew Singh <mattsingh@hotmail.com>
2021-11-14 05:54:38 -0500

    what the fuck

--- app/assets/components/GetEventsNearMe.js
+++ app/assets/components/GetEventsNearMe.js
@@ -1,58 +1,62 @@
+import * as Location from 'expo-location';
+import path from './Path';
+
 const axios = require('axios').default;
 axios.defaults.baseURL = path();
 
 //set the distance to -1 if you want to get all events
-async function getEventsNearMe(distance, tags) { 
-    if (Location.hasServicesEnabledAsync()) {
-        var coordinates = Location.getLastKnowPositionAsync();
-    } else { 
-        distance = -1;
-    }
+export async function getEventsNearMe(distance, tags) {
+	if (await Location.hasServicesEnabledAsync()) {
+		var coordinates = await Location.getLastKnownPositionAsync();
+	} else {
+		distance = -1;
+	}
 
-    tags = tags.join(',');
-    coordinates = coordinates.coords.latitude + ',' + coordinates.coords.longitude;
-    var queryParams = '?';
-    var setAnd = false;
-    if (distance != -1) {
-        queryParams += distance;
-        setAnd = true;
-    }
-    if (tags) {
-        if (setAnd) {
-            queryParams += '&';
-        }
-        queryParams += tags;
-    }
-    var eventsIDs = []
-    try {
-        const res = await axios.get('http://localhost:3000/events/near-me/' + coordinates + queryParams);
-        // console.log(res.data.eventsNearMe)
-        eventsIDs = res.data.eventsNearMe;
-    } catch (err) {
-        console.log(err);
-        return;
-    } finally {
-        console.log(eventsIDs)
-        const eventsData = await getEventInfo(eventsIDs);
-        console.log(eventsData);
-    }
+	tags = tags.join(',');
+	coordinates =
+		coordinates.coords.latitude + ',' + coordinates.coords.longitude;
+	var queryParams = '';
+	var setAnd = false;
+	if (distance != -1) {
+		queryParams += '?' + distance;
+		setAnd = true;
+	}
+	if (tags) {
+		if (setAnd) {
+			queryParams += '&';
+		} else {
+			queryParams += '?';
+		}
+		queryParams += tags;
+	}
+	var eventsIDs = [];
+	try {
+		const res = await axios.get(
+			'/events/near-me/' + coordinates + queryParams
+		);
+		eventsIDs = res.data.eventsNearMe;
+	} catch (err) {
+		console.log(err);
+		return;
+	} finally {
+		// console.log(eventsIDs);
+		const eventsData = await getEventInfo(eventsIDs);
+		// console.log(eventsData);
+	}
 }
 
-async function getEventInfo(eventIDs) {
-    const events = [];
-    try {
-        for (let i = 0; i < eventIDs.length; i++) {
-
-            const id = eventIDs[i];
-            const res = await axios.get('http://localhost:3000/events/' + id);
-            events.push(res.data);
-            
-        }
-    } catch (err) {
-        console.log(err);
-        return;
-    } finally {
-        return events;
-    }
+export async function getEventInfo(eventIDs) {
+	const events = [];
+	try {
+		for (let i = 0; i < eventIDs.length; i++) {
+			const id = eventIDs[i];
+			const res = await axios.get('/events/' + id);
+			events.push(res.data);
+		}
+	} catch (err) {
+		console.log(err);
+		return;
+	} finally {
+		return events;
+	}
 }
-getEventsNearMe(distance, tags, cords);
--- app/assets/screens/ExploreScreen.js
+++ app/assets/screens/ExploreScreen.js
@@ -1,14 +1,108 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+	ScrollView,
+	StyleSheet,
+	View,
+	TextInput,
+	Text,
+	FlatList,
+	Image,
+	ImageBackground,
+	TouchableOpacity,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import path from '../components/Path';
+import { getEventsNearMe, getEventInfo } from '../components/GetEventsNearMe';
+
+var events = null;
 
 function ExploreScreen(props) {
-	return <View style={styles.container} />;
+	(async () => {
+		events = await getEventsNearMe(-1, []);
+	})();
+
+	return (
+		<View style={styles.container}>
+			<View>
+				<TextInput style={styles.searchBox} />
+			</View>
+			<Text>Events Near Me</Text>
+			<FlatList
+				horizontal={true}
+				data={events}
+				keyExtractor={(item) => item.eventID}
+				renderItem={({ item }) => {
+					return (
+						<View>
+							<TouchableOpacity>
+								<Image
+									source={require('../sample-event-photos/photo-2.jpeg')}
+									style={{
+										width: 150,
+										height: 150,
+										marginRight: 8,
+										borderRadius: 10,
+									}}
+								/>
+								<View style={{ flexDirection: 'row' }}>
+									<Feather
+										style={styles.imageLocationIcon}
+										name='map-pin'
+										size={16}
+										color='white'
+									/>
+									<Text style={styles.imageText}>
+										{item.eventName}
+									</Text>
+								</View>
+							</TouchableOpacity>
+						</View>
+					);
+				}}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: 'blue',
+		backgroundColor: 'gold',
+	},
+	searchContainer: {
+		paddingTop: 100,
+		paddingLeft: 16,
+	},
+	searchBox: {
+		marginTop: 16,
+		backgroundColor: '#fff',
+		paddingLeft: 24,
+		padding: 12,
+		borderTopRightRadius: 40,
+		borderBottomRightRadius: 40,
+		width: '90%',
+	},
+	card: {
+		backgroundColor: 'grey',
+		borderRadius: 20,
+		padding: 20,
+		marginVertical: 8,
+		marginHorizontal: 10,
+		height: 100,
+	},
+	imageLocationIcon: {
+		position: 'absolute',
+		marginTop: 4,
+		left: 10,
+		bottom: 10,
+	},
+	imageText: {
+		position: 'absolute',
+		marginTop: 4,
+		left: 30,
+		bottom: 10,
+		fontSize: 14,
+		color: 'white',
 	},
 });
 
