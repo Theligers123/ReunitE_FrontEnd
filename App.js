977c1ca9eb125ad0040c0813907287e3f56298e6
Ahmad Louis <ahmadlouis35@gmail.com>
2021-11-13 23:07:05 -0500

    Temp Event maker push

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import WelcomeScreen from './app/assets/screens/WelcomeScreen';
 import Profile from './app/assets/screens/Profile';
+import CreateMeeting from './app/assets/screens/CreateMeeting';
 
 export default function App() {
     return (
-       <Profile/>
+      //  <Profile/>
       // <WelcomeScreen/>
+      <CreateMeeting/>
     );
   }
